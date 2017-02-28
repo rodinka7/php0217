9e4c6554382a92aa4f3eddd950ebf61e6efdf654
rodinka7 <rodinka7@gmail.com>
2017-02-28 16:29:54 +0200

    Hometask 2 -> developping...

--- 02/index.php
+++ 02/index.php
@@ -2,7 +2,7 @@
 /* TASK 1 */
 $arr = ['Hello, world!', 'Today is a wonderful day!', 'The weather is perfect'];
 
-function showStr($arr, $param) {
+function showStr($arr, $param = false) {
 	for ($i = 0; $i < count($arr); $i++) {
 		echo "<p>{$arr[$i]}</p>";
 	}
@@ -19,48 +19,72 @@ echo '<hr>';
 
 /* TASK 2 */
 function countArr($arr, $str) {
-	$result = $arr[0];
-
 	if (!is_array($arr)) {
-		echo 'Выведите массив чисел!';
+		echo 'Введите массив чисел! <br />';
 	}
 
+	for ($i = 0; $i < count($arr); $i++) {
+		if (!is_int($arr[$i])) {
+			echo 'В массиве должны быть только числа! <br />';
+		}
+	}
 	switch ($str){
-		case 'сложение':
-			for ($i = 1; $i <= count($arr); $i++) {
+		case '+':
+		    $result = 0;
+			for ($i = 0; $i <= count($arr); $i++) {
 				$result += $i; 
 			}
 			break;
-		case 'вычитание':
+		case '-':
+            $result = $arr[0];
 			for ($i = 1; $i <= count($arr); $i++) {
 				$result -= $arr[$i];
 			}
 			break;
-		case 'умножение':
-			for ($i = 1; $i < count($arr); $i++) {
+		case '*':
+            $result = 1;
+			for ($i = 0; $i < count($arr); $i++) {
 				$result *= $arr[$i];
 			}
 			break;
-		case 'деление':
+		case '/':
+            $result = $arr[0];
 			for ($i = 1; $i < count($arr); $i++) {
 				if ($arr[$i] === 0) {
 					echo 'На ноль делить нельзя!';
 					break;
 				};
-
-				$result /= $arr[$i];
+                $result /= $arr[$i];
 			}
 			break;
-		case 'возведение в степень':
+		case '**':
+            $result = $arr[0];
 			for ($i = 1; $i < count($arr); $i++) {
 				$result = pow($result, $arr[$i]);
-				echo $result;
 			}
 	}
-
-	echo "result = $result";
-}	
-
-countArr([5,2,4], 'возведение в степень');
+	if (is_int($result)) {
+        echo "result = $result <br />";
+    }
+}
+countArr([2,0,5], '/');
+echo '<hr>';
 /* TASK 2 */
+
+/* TASK 3 */
+    function calcEverything($str, ...$numbers) {
+        for ($i = 0; $i < count($numbers); $i++) {
+            if (!is_int($numbers[$i]) && !is_float($numbers[$i])) {
+                echo 'Введите целые или вещественные числа!';
+            }
+        };
+
+        switch ($str) {
+            case '+':
+                $result += $arr[i];
+        }
+    }
+
+    calcEverything('dfd', 1,2,3,4);
+/* TASK 3 */
 ?>
